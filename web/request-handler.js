418f50ae835d89f8651ab5a538fbe7f61c8b09d6
darkstar21 <mikeh09@gmail.com>
2014-10-01 14:56:34 -0700

    first test passed and archive pulled

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,18 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('./http-helpers.js')
 // require more modules/folders here!
 
+
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, httpHelper.headers)
+    httpHelper.serveAssets(res, "./web/public/index.html", "write")
+    res.end()
+  }
+
 };
+  // res.end(archive.paths.list);
+
